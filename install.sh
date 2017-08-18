5b6a1144a77ac10c0ca31e9df766d6b22d62a460
paul-baranowski <p.baranowski@bigspaceship.com>
2017-08-18 16:40:13 -0400

    added some shell config to test for env before install

--- install.sh
+++ install.sh
@@ -1,20 +1,34 @@
 set -e
 
-# Install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Check if homebrew is installed and if not install it
+which -s brew
+if [[ $? !=0 ]] ; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else 
+  brew update
+fi
 
 #Brew specific installs
-brew install git
-brew install node
-sudo apt-get install zsh
+# -- Check if Git is installed
+echo "Checking if git is installed"
+which -s git || brew install git
+# -- Check if node is installed
+echo "Checking for node"
+node --version
+if [[ $? != 0 ]] ; then
+  brew install node
+fi
+# -- Install zsh
+echo "Checking zsh"
+which -s zsh || brew install zsh
 
 # Change shell to zsh
+echo "Changing shell to zsh"
 command -v zsh | sudo tee -a /etc/shells
 sudo chsh -s $(which zsh)
 
 # Install zsh theme
 npm install -g spaceship-zsh-theme
-
 
 echo -n "SymLinking dotfiles..."
 #Symlink .zshrc and .vimrc
